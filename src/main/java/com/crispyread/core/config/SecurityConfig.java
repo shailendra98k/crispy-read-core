05155beb88e59171b516a0172719f5d3d0e96df4
shailendra98k <shailendra98k@gmail.com>
2024-07-26 16:03:02 +0530

    Secured PUT and POST call with Basic Authentication

---
+++ src/main/java/com/crispyread/core/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.crispyread.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
